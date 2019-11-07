ca85a444aa46f69d2c498ede9b3d2686738e76c1
baurik82 <baurik82@mail.ru>
2019-11-07 15:50:10 +0600

    Add project files.

---
+++ EPAM_Task16/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EPAM_Task16
+{
+    class Program
+    {
+        static int n;
+
+        static void Main(string[] args)
+        {
+            //SumNumbersAsync().Wait();
+            SetIntervalAsync().Wait();
+        }
+
+        static async Task SumNumbersAsync()
+        {
+            await Task.Run(() =>
+            {
+                if (n == 0)
+                {
+                    n = Int32.Parse(Console.ReadLine());
+                }
+               
+                int sum = 0;
+
+                for (int i = 0; i <= n; i++)
+                {
+                    sum += i;
+                    Thread.Sleep(1000);
+                }
+
+                Console.Write($"Сумма чисел равна {sum}");
+            });
+        }
+
+        static async Task SetIntervalAsync()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("Введите предельное число для суммирования: ");
+                n = Int32.Parse(Console.ReadLine());
+
+                while (true)
+                {
+                    if (n != 0)
+                    {
+                        Console.WriteLine("Измените предельное число для суммирования: ");
+                        n = Int32.Parse(Console.ReadLine());
+                        SumNumbersAsync();
+                    }
+                }
+            });
+        }
+    }
+}
